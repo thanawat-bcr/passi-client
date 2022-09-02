b9301fe56428a4ecf2c1eef2c6d5f0f137b6f670
thanawat.bcr <thanwat.bcr@gmail.com>
2022-09-02 16:39:43 +0700

    axios kairos

--- pages/kairos.vue
+++ pages/kairos.vue
@@ -2,25 +2,30 @@
 .kairos.container.mx-auto
   h1.text-primary-500.font-bold KAIROS API TESTER
   .w-full.flex.flex-col.items-center.gap-y-4.my-4
-    .w-64.h-64.border.bg-center.bg-cover(:style="{ 'background-image': `url(${img})` }")
-    input(type="file" @change="onChange" ref="fileInput")
+    .w-64.h-64.bg-green-50.relative.border.rounded-lg
+      input(type="file" @change="onChange" ref="fileInput").absolute.inset-0.opacity-0
+      .w-64.h-64.bg-center.bg-cover(:style="{ 'background-image': `url(${img})` }")
+    .flex.gap-x-2
+      SoButton(v-for="s in subjects" :key="s" :mode="subject === s ? '' : 'outline'" @click="subject = s") {{ s }}
   .w-full.grid.grid-cols-2.gap-x-2
     SoButton(block @click="onEnroll") Enroll
     SoButton(block @click="onVerify") Verify
-
 </template>
 
 <script>
 import { defineComponent, ref } from '@nuxtjs/composition-api';
 import { axios } from '@/use/useAxios';
 
-const GALLERY_NAME = 'passi';
+const GALLERY_NAME = 'passi-test';
 
 const kairos = defineComponent({
   setup() {
     const images = ref(null);
     const img = ref(null);
     const fileInput = ref('');
+
+    const subjects = ref(['tutorism', 'fluke', 'jimmy']);
+    const subject = ref('tutorism');
 
     const onChange = (e) => {
       images.value = e.target.files[0];
@@ -37,11 +42,12 @@ const kairos = defineComponent({
     const onEnroll = async () => {
       const formData = new FormData();
       formData.append('image', images.value)
-      formData.append('name', 'tutorism')
+      formData.append('subject_id', subject.value)
       formData.append('gallery_name', GALLERY_NAME)
+      console.log('Enroll:', subject.value)
       try {
-        const res = await axios.post('/enroll', formData)
-        console.log(res)
+        const res = await axios.post('/kairos/enroll', formData)
+        console.log('data:', res.data)
       }catch(err) {
         console.log(err)
       }
@@ -50,18 +56,21 @@ const kairos = defineComponent({
     const onVerify = async () => {
       const formData = new FormData();
       formData.append('image', images.value)
-      formData.append('name', 'tutorism')
+      formData.append('subject_id', subject.value)
       formData.append('gallery_name', GALLERY_NAME)
+      console.log('Verify:', subject.value)
       try {
-        const res = await axios.post('/verify', formData)
-        const { confidence, status } = res.data.images[0].transaction
-        console.log('confidence:', confidence, 'status:', status)
+        const res = await axios.post('/kairos/verify', formData)
+        console.log('data:', res.data)
       }catch(err) {
         console.log(err)
       }
     }
 
     return {
+      subjects,
+      subject,
+
       img,
       fileInput,
       onChange,
--- use/useAxios.ts
+++ use/useAxios.ts
@@ -4,16 +4,19 @@ import { default as axiosLib, AxiosRequestConfig } from 'axios';
 // import { useAxios as vueUseUseAxios } from '@vueuse/integrations';
 
 const BASE_URL = 'http://localhost:3128/'
+// const BASE_URL = 'https://api.kairos.com'
 
 const instance = axiosLib.create({
   baseURL: BASE_URL,
 })
 
-// instance.interceptors.request.use((config) => {
-//   config.headers.common['Authorization'] = `Bearer ${process.browser ? localStorage.getItem('token') : ''}`;
-//   config.headers.common['access-control-allow-origin'] = `*`;
-//   return config;
-// });
+instance.interceptors.request.use((config) => {
+  // config.headers.common['app_id'] = '68b0c0ea';
+  // config.headers.common['app_key'] = '3e1615c6719a7b955cb417ba8045f4e1';
+  // config.headers.common['Authorization'] = `Bearer ${process.browser ? localStorage.getItem('token') : ''}`;
+  // config.headers.common['access-control-allow-origin'] = `*`;
+  return config;
+});
 
 // instance.interceptors.response.use((response) => {
 //   return response;
