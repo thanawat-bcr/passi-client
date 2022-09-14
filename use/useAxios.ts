4102860f83dd282f186adb3813283b5dd773558f
thanawat.bcr <thanwat.bcr@gmail.com>
2022-09-14 21:09:43 +0700

    fix: axios url

--- use/useAxios.ts
+++ use/useAxios.ts
@@ -3,9 +3,9 @@
 import { default as axiosLib } from 'axios';
 // import { useAxios as vueUseUseAxios } from '@vueuse/integrations';
 
-const BASE_URL = 'http://localhost:8080/'
+// const BASE_URL = 'http://localhost:8080/'
 // const BASE_URL = 'http://passi.sit.kmutt.ac.th:3128/'
-// const BASE_URL = 'https://passi-api.tutorism.me:8443/'
+const BASE_URL = 'https://passi-api.tutorism.me:8443/'
 // const BASE_URL = 'https://api.kairos.com'
 
 const instance = axiosLib.create({
